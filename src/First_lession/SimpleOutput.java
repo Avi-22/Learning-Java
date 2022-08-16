6bde256db821f6f672233f04661a768bc1700d9a
Avi-22 <96301038+Avi-22@users.noreply.github.com>
2022-08-16 10:32:18 +0300

    2

---
+++ src/First_lession/SimpleOutput.java
@@ -0,0 +1,6 @@
+public class SimpleOutput {
+    public static void main(String[] args) throws Exception {
+        String str = "My first Java application!";
+        System.out.println(str);
+    }
+}
---
+++ src/First_lession/Variables/Assignment_1.java
@@ -0,0 +1,13 @@
+package First_lession.Variables;
+
+public class Assignment_1 {
+    public static void main() {
+        int firs_value= 1, second_value = 2, third_value =3;
+        double fourth_value = 3.14; 
+        System.out.println(firs_value);
+
+
+
+    }
+    
+}
