cdbd55dc4b231b13a647d4e601e2e02b84686bfa
Avi-22 <96301038+Avi-22@users.noreply.github.com>
2022-08-17 10:16:53 +0300

    5

--- First_Lesson/Expressions/Assignment_1.java
+++ First_Lesson/Expressions/Assignment_1.java
@@ -1,3 +1,7 @@
 public class Assignment_1 {
-    
+  public static void main(String[] args) throws Exception {
+    byte integer = 50;
+    System.out.println(integer%10);
+    System.out.println((integer-integer%10)/10);
+  } 
 }
