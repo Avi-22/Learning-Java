ce7b4d6179d444ebd3fbfc4589f135b3d30c22f3
Avi-22 <96301038+Avi-22@users.noreply.github.com>
2022-08-21 07:18:38 +0300

    Update test.java

--- test.java
+++ test.java
@@ -18,36 +18,43 @@ public class test {
 
     int firstGroupChoice, secondGroupChoice, thirdGroupChoice;
 
+    String invalid = "Your chice is invalid";
+    String choice_0_2 = "Enter yoer choice 0-2";
+    String choice_0_3 = "Enter yoer choice 0-3";
+    String first_group = "The firs group is: I = 0 , You = 1, We = 2, They = 3.";
+    String second_group = "The secound gruop is: am = 0, is = 1, are = 2. ";
+    String third_group = "The third group id: happy = 0, sad = 1, tired = 2, thrilled = 3.";
+
     System.out.println("Enter Your Name");
     String userName = name.nextLine();
-    System.out.println("Hello " + userName + "you have a choice to build your on sentence.");
-    System.out.println("The firs group is: I = 0 , You = 1, We = 2, They = 3.");
-    System.out.println("Enter yoer choice 0-3");
+    System.out.println("Hello " + userName + " you have a choice to build your on sentence.");
+    System.out.println(first_group);
+    System.out.println(choice_0_3);
     firstGroupChoice = firs.nextInt();
     if (firstGroupChoice > 3 || firstGroupChoice < 0) {
-      System.out.println("Your chice is invalid");
-      System.out.println("The firs group is: I = 0 , You = 1, We = 2, They = 3.");
-      System.out.println("Enter yoer choice 0-3");
+      System.out.println(invalid);
+      System.out.println(first_group);
+      System.out.println(choice_0_3);
       firstGroupChoice = firs.nextInt();
     }
 
-    System.out.println("The secound gruop is: am = 0, is = 1, are = 2. ");
-    System.out.println("Enter your choice 0-2");
+    System.out.println(second_group);
+    System.out.println(choice_0_2);
     secondGroupChoice = second.nextInt();
     if (secondGroupChoice > 2 || secondGroupChoice < 0) {
-      System.out.println("Your choice is invalid");
-      System.out.println("The secound gruop is: am = 0, is = 1, are = 2. ");
-      System.out.println("Enter your choice 0-2");
+      System.out.println(invalid);
+      System.out.println(second_group);
+      System.out.println(choice_0_2);
       secondGroupChoice = second.nextInt();
     }
 
-    System.out.println("The third group id: happy = 0, sad = 1, tired = 2, thrilled = 3.");
-    System.out.println("Enter yoer choice 0-3");
+    System.out.println(third_group);
+    System.out.println(choice_0_3);
     thirdGroupChoice = third.nextInt();
     if (thirdGroupChoice > 3 || thirdGroupChoice < 0) {
-      System.out.println("Your choice is invalid");
-      System.out.println("The third group id: happy = 0, sad = 1, tired = 2, thrilled = 3.");
-      System.out.println("Enter yoer choice 0-3");
+      System.out.println(invalid);
+      System.out.println(third_group);
+      System.out.println(choice_0_3);
       thirdGroupChoice = third.nextInt();
     }
 
