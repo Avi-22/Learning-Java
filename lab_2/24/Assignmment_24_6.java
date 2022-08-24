668962cb910bf28bdcbed7d535c1e05c8655a2e1
Avi-22 <96301038+Avi-22@users.noreply.github.com>
2022-08-24 18:31:04 +0300

    .

---
+++ lab_2/24/Assignmment_24_6.java
@@ -0,0 +1,18 @@
+public class Assignmment_24_6 {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int i = 1;
+        int sum=0;
+
+
+        do{
+            if (i%2 != 0){
+                System.out.println(i);
+                sum= sum +i;
+            }
+
+            i++;
+        }while(i<=9);
+        System.out.println("The sum of all odd numbers: "+sum);
+    }
+}
--- test_2.java
+++ test_2.java
@@ -5,28 +5,8 @@ public class test_2 {
      * @param args
      */
     public static void main(String[] args) {
-
-        int first_num, second_num;
-        boolean are_equal;
-        String operatiion = args[1];
-        first_num = Integer.parseInt(args[0]);
-        second_num = Integer.parseInt(args[2]);
-
-        are_equal = operatiion.equals("+");
-        if (are_equal) {
-            System.out.println(first_num + second_num);
-        }
-        are_equal = operatiion.equals("*");
-        if (are_equal) {
-            System.out.println(first_num * second_num);
-        }
-        are_equal = operatiion.equals("-");
-        if (are_equal) {
-            System.out.println(first_num - second_num);
-        }
-        are_equal = operatiion.equals("/");
-        if (are_equal) {
-            System.out.println(first_num / second_num);
-        }
+      
+       
+    
     }
 }
