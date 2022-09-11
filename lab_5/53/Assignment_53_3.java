9d3b49a5071cd528e847ac7f56079e53172beeea
Avi-22 <96301038+Avi-22@users.noreply.github.com>
2022-09-11 18:28:26 +0300

    1

--- lab_5/53/Assignment_53_3.java
+++ lab_5/53/Assignment_53_3.java
@@ -1,33 +1,20 @@
 public class Assignment_53_3 {
-    public static boolean is_duplicate(int[] arr, int num) {
-        boolean duplicate = false;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
-                duplicate = true;
-                break;
-            }
-        }
-        return duplicate;
-    }
-
     public static int[] max_to_min(int[] array) {
-        int[] max_2_min = new int[array.length];
-        int max_num = array[0];
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++) {
-                if (max_num < array[j]) {
-                    max_num = array[j];
-                    max_2_min[i] = array[j];
+    int middel;
+        for(int i=0;i<array.length;i++){
+            for(int j=i;j<array.length;j++){
+                if (array[i]< array[j]){
+                    middel= array[i];
+                    array[i]=array[j];
+                    array[j]=middel;
                 }
-            }
+            } 
         }
-
-        return max_2_min;
+        return array;
     }
 
     public static void main(String[] args) {
-        int[] array = { 1, 4, 6, 88, 6, 99 };
+        int[] array = { 1, 2, 3, 4, 5, 99 };
         int[] max = max_to_min(array);
         for (int i = 0; i < max.length; i++) {
             System.out.print(max[i] + ", ");
